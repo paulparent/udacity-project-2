808023f204a3fd60a4f82db3e106f5e2358e4623
Paul Parent <paul.parent@gmail.com>
2015-10-16 15:37:33 -0700

    tournament.py completed first several functions (in order); python work halted to work through playerStandings SQL details
    tournament.sql updated to include Views for player matches, player wins, and player standings; also added insert statements to add test data
    
    tournament_test.py should only have been updated to reflect uncommenting of function calls as progress made against prescribed python functions

--- tournament.py
+++ tournament.py
@@ -4,34 +4,69 @@
 #
 
 import psycopg2
-
+import time
 
 def connect():
-    """Connect to the PostgreSQL database.  Returns a database connection."""
+    """Connect to the PostgreSQL database. Returns a database connection."""
     return psycopg2.connect("dbname=tournament")
 
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    DB = connect()
+    c = DB.cursor()
+    c.execute("DELETE FROM match")
+
+    DB.commit()
+    DB.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    DB = connect()
+    c = DB.cursor()
+    c.execute("DELETE FROM player")
+
+    DB.commit()
+    DB.close()
 
 
 def countPlayers():
-    """Returns the number of players currently registered."""
+    """Returns the number of players currently registered.
+
+    (The database should count the players.)
+    """
+    DB = connect()
+    c = DB.cursor()
+
+    c.execute("SELECT COUNT(*) as NUM FROM player")
+    count = c.fetchone()
+
+    DB.close()
+    return count[0]
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
   
-    The database assigns a unique serial id number for the player.  (This
+    The database assigns a unique serial id number for the player. (This
     should be handled by your SQL database schema, not in your Python code.)
   
     Args:
       name: the player's full name (need not be unique).
     """
+    DB = connect()
+    c = DB.cursor()
+
+    c.execute("INSERT INTO player (name) VALUES (%s)", (name,))
+    DB.commit()
+
+    c.execute("SELECT id FROM player WHERE name = (%s) ORDER BY id DESC", (name,))
+    newID = c.fetchone()
+    c.execute("INSERT INTO player_standing (player_id, tournament_id, "
+              "points_total, bye_count) VALUES (%s,1,0,0)", (newID[0],))
+    DB.commit()
+    DB.close()
 
 
 def playerStandings():
@@ -46,15 +81,36 @@ def playerStandings():
         name: the player's full name (as registered)
         wins: the number of matches the player has won
         matches: the number of matches the player has played
+
+    c.execute("SELECT player_id, p.name, COUNT(r.result), COUNT(r.player_id) "
+                "FROM player_standing s "
+                "JOIN match_result r USING (player_id) "
+                "JOIN player p ON s.player_id = p.id "
+                "GROUP BY player_id "
+                "ORDER BY s.points_total DESC")
+
     """
+    DB = connect()
+    c = DB.cursor()
+
+    c.execute("SELECT p.id, p.name, r.result, s.bye_count "
+              "FROM player p "
+              "LEFT JOIN player_standing s ON p.id = s.player_id "
+              "LEFT JOIN match_result r ON p.id = r.player_id "
+              "ORDER BY s.points_total")
+    standings = c.fetchall()
+    print standings
+
+    DB.close()
+    return standings
 
 
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
 
     Args:
-      winner:  the id number of the player who won
-      loser:  the id number of the player who lost
+      winner: the id number of the player who won
+      loser: the id number of the player who lost
     """
  
  
@@ -62,7 +118,7 @@ def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
   
     Assuming that there are an even number of players registered, each player
-    appears exactly once in the pairings.  Each player is paired with another
+    appears exactly once in the pairings. Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
   
@@ -73,5 +129,4 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
 
--- tournament.sql
+++ tournament.sql
@@ -1,40 +1,35 @@
--- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use them.
---
--- Database Review Criteria:
--- -------------------------
--- Meaningful names for tables and columns
--- No unnecessary tables or columns
--- Columns are the proper data type
--- Views used to make queries more concise (Extra Credit)
--- Properly specified primary and foreign keys (Extra Credit)
---
---
--- Quick Database Model Representation:
--- ------------------------------------
--- player (id**: SERIAL, name: TEXT)
--- tournament (id**: SERIAL, name: TEXT, start_date: DATE)
--- match (id**: SERIAL, player_id_1*: INT, player_id_2*: INT,
---  tournament_id*: INT, match_start: TIMESTAMP)
--- match_result (match_id**: INT, player_id**: INT, result: TEXT)
--- player_standing (player_id**: INT, tournament_id**: INT, points_total: INT,
---  bye_count: INT)
+/* 
+  Table definitions for the tournament project
 
+  Put your SQL 'create table' statements in this file; also 'create view'
+  statements if you choose to use them.
 
+  Database Review Criteria:
+  -------------------------
+  Meaningful names for tables and columns
+  No unnecessary tables or columns
+  Columns are the proper data type
+  Views used to make queries more concise (Extra Credit)
+  Properly specified primary and foreign keys (Extra Credit)
+
+  Quick Database Model Representation:
+  ------------------------------------
+  player (id**: SERIAL, name: TEXT)
+  tournament (id**: SERIAL, name: TEXT, start_date: DATE)
+  match (id**: SERIAL, player_id_1*: INT, player_id_2*: INT,
+  tournament_id*: INT, match_start: TIMESTAMP)
+  match_result (match_id**: INT, player_id**: INT, result: TEXT)
+  player_standing (player_id**: INT, tournament_id**: INT, points_total: INT,
+  bye_count: INT)
+*/
 
 -- used during development to ensure clean start
 DROP DATABASE IF EXISTS tournament;
--- DROP TABLE IF EXISTS player,
---  tournament,
---  match,
---  match_result,
---  player_standing
---  CASCADE;
-
 CREATE DATABASE tournament;
 \c tournament;
+
+-- enumerate types of match results to ensure data integrity
+CREATE TYPE result_type AS ENUM ('Win', 'Loss', 'Draw', 'Bye');
 
 CREATE TABLE player(
   id SERIAL PRIMARY KEY,
@@ -49,23 +44,133 @@ CREATE TABLE tournament(
 
 CREATE TABLE match(
   id SERIAL PRIMARY KEY,
-  player_id_1 INT REFERENCES player NOT NULL,
-  player_id_2 INT REFERENCES player NOT NULL,
-  tournament_id INT REFERENCES tournament NOT NULL,
+  player_id_1 INT REFERENCES player ON DELETE CASCADE NOT NULL,
+  player_id_2 INT REFERENCES player ON DELETE CASCADE NOT NULL,
+  tournament_id INT REFERENCES tournament ON DELETE CASCADE NOT NULL,
   match_start TIMESTAMP NOT NULL
 );
 
 CREATE TABLE match_result(
-  match_id INT REFERENCES match NOT NULL,
-  player_id INT REFERENCES player NOT NULL,
-  result TEXT NOT NULL,
+  match_id INT REFERENCES match ON DELETE CASCADE NOT NULL,
+  player_id INT REFERENCES player ON DELETE CASCADE NOT NULL,
+  result result_type NOT NULL,
   PRIMARY KEY (match_id, player_id)
 );
 
 CREATE TABLE player_standing(
-  player_id INT REFERENCES player NOT NULL,
-  tournament_id INT REFERENCES tournament NOT NULL,
+  player_id INT REFERENCES player ON DELETE CASCADE NOT NULL,
+  tournament_id INT REFERENCES tournament ON DELETE CASCADE NOT NULL,
   points_total INT NOT NULL,
   bye_count INT NOT NULL,
   PRIMARY KEY (player_id, tournament_id)
 );
+
+CREATE VIEW player_wins AS
+  SELECT p.id, p.name, COUNT(r.result) AS "total wins"
+  FROM player p
+    LEFT JOIN match_result r ON p.id = r.player_id
+  WHERE r.result = 'Win'
+  GROUP BY p.id
+  ORDER BY COUNT(r.result) DESC;
+
+CREATE VIEW player_matches AS
+  SELECT p.id, p.name, COUNT(r.result) AS "total matches"
+  FROM player p
+    LEFT JOIN match_result r ON p.id = r.player_id
+  GROUP BY p.id
+  ORDER BY COUNT(r.result) DESC;
+
+CREATE VIEW current_standings AS
+  SELECT pm.id, pm.name, pw."total wins", pm."total matches"
+  FROM player_matches pm
+    LEFT JOIN player_wins pw ON pm.id = pw.id;
+
+-- FUCK YEAH: SELECT * FROM current_standings; is working in psql
+-- Tomorrow: return to Python to add call to this final view
+-- Add code to view to ensure rank by total wins conforms to instructions
+
+/* Added to satisfy testStandingBeforeMatches() test in tournament_test.py.
+  Ensures first tournament created is ready and can be assigned to
+  new players, because test requires new players to have player standing
+  and player standing requires a tournament.
+  It would be better to not expect new players to have a player standing,
+  but I am proceeding under the assumption that I cannot change the test. */
+INSERT INTO tournament (id, name, start_date) VALUES (1, 'Default Tournament', '2015-01-01');
+
+/* TEST DATA to Populate Database */
+INSERT INTO tournament (id, name, start_date) VALUES (2, 'Run the Jewels', '2015-10-15');
+
+INSERT INTO player (id, name) VALUES (1, 'Paul');
+INSERT INTO player (id, name) VALUES (2, 'RLW');
+INSERT INTO player (id, name) VALUES (3, 'JZ');
+INSERT INTO player (id, name) VALUES (4, 'ABro');
+
+INSERT INTO match (id, player_id_1, player_id_2, tournament_id, match_start) 
+  VALUES (1, 1, 2, 2, '2015-10-15 12:34:56');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (1, 1, 'Win');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (1, 2, 'Loss');
+
+INSERT INTO match (id, player_id_1, player_id_2, tournament_id, match_start)
+  VALUES (2, 1, 3, 2, '2015-10-15 12:34:56');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (2, 1, 'Win');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (2, 3, 'Loss');
+
+INSERT INTO match (id, player_id_1, player_id_2, tournament_id, match_start)
+  VALUES (3, 1, 4, 2, '2015-10-15 12:34:56');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (3, 1, 'Win');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (3, 4, 'Loss');
+
+INSERT INTO match (id, player_id_1, player_id_2, tournament_id, match_start)
+  VALUES (4, 2, 1, 2, '2015-10-15 12:34:56');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (4, 2, 'Win');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (4, 1, 'Loss');
+
+INSERT INTO match (id, player_id_1, player_id_2, tournament_id, match_start)
+  VALUES (5, 2, 3, 2, '2015-10-15 12:34:56');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (5, 2, 'Win');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (5, 3, 'Loss');
+
+INSERT INTO match (id, player_id_1, player_id_2, tournament_id, match_start)
+  VALUES (6, 2, 4, 2, '2015-10-15 12:34:56');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (6, 2, 'Win');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (6, 4, 'Loss');
+
+INSERT INTO match (id, player_id_1, player_id_2, tournament_id, match_start)
+  VALUES (7, 3, 1, 2, '2015-10-15 12:34:56');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (7, 3, 'Win');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (7, 1, 'Loss');
+
+INSERT INTO match (id, player_id_1, player_id_2, tournament_id, match_start)
+  VALUES (8, 3, 2, 2, '2015-10-15 12:34:56');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (8, 3, 'Win');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (8, 2, 'Loss');
+
+INSERT INTO match (id, player_id_1, player_id_2, tournament_id, match_start)
+  VALUES (9, 3, 4, 2, '2015-10-15 12:34:56');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (9, 3, 'Win');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (9, 4, 'Loss');
+
+INSERT INTO match (id, player_id_1, player_id_2, tournament_id, match_start)
+  VALUES (10, 4, 1, 2, '2015-10-15 12:34:56');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (10, 4, 'Win');
+INSERT INTO match_result (match_id, player_id, result)
+  VALUES (10, 1, 'Loss');
