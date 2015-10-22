76564ec19189411ea1f1c4620c1fdc7b9600f267
Paul Parent <paul.parent@gmail.com>
2015-10-22 15:49:06 -0700

    edits to comments and formatting clean up in preparation for submission

--- tournament.py
+++ tournament.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
-# 
-# tournament.py -- implementation of a Swiss-system tournament
 #
+# tournament.py -- implementation of a Swiss-system tournament
 
 import psycopg2
 import time
 from datetime import datetime
 
 def connect():
-    """Connect to the PostgreSQL database. Returns a database connection."""
+    """Connect to the PostgreSQL database. 
+
+    Returns: a database connection object
+    """
     return psycopg2.connect("dbname=tournament")
 
 
@@ -33,9 +35,10 @@ def deletePlayers():
 
 
 def countPlayers():
-    """Returns the number of players currently registered.
+    """Queries the database for the count of currently registered players.
 
-    (The database should count the players.)
+    Returns: count, an integer indicating the number of players currently
+        registered.
     """
     DB = connect()
     c = DB.cursor()
@@ -48,11 +51,9 @@ def countPlayers():
 
 
 def registerPlayer(name):
-    """Adds a player to the tournament database.
-  
-    The database assigns a unique serial id number for the player. (This
-    should be handled by your SQL database schema, not in your Python code.)
-  
+    """Adds a player to the tournament database. The database assigns a unique
+        serial id number for the player.
+
     Args:
       name: the player's full name (need not be unique).
     """
@@ -66,9 +67,6 @@ def registerPlayer(name):
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
-
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
 
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
@@ -86,49 +84,29 @@ def playerStandings():
 
     DB.close()
 
-    # I think this code is no longer required after figuring out how to
-    # handle null values at the database level.
-
-    ##### Delete prior to submitting final project. ####
-
-    # Walk through each tuple in standings list to check for null values
-    # and replace null values ('None') with 0
-    # standings_clean = []
-    # for (p_id, name, wins, matches) in standings:
-    #     new_list = []
-    #     if wins == None:
-    #         new_list = [p_id, name, 0, matches]
-    #     else:
-    #         new_list = [p_id, name, wins, matches]
-    #     tuple(new_list) # re-cast list as tuple to be added back to list
-    #     standings_clean.append(new_list)
-
-    # return standings_clean
-
     return standings
 
-# also need overloaded version with (match_id, player1, result_p1, player2, 
-    # result_p2) to account for results for prescheduled matches and draws / byes
+
 def reportMatch(winner, loser):
     """
     Records the outcome of a single match between two players. Because no
-    match_id indicated, first creates new match linked to default entry
-    for tournament table ('no tournament').
+    test does not supply a match_id, this function first creates new match
+    linked to the default record in the tournament table ('no tournament').
 
-    Full match set of records for a match consists of record for
-    match (id, player_id_1, player_id_2, tournament_id, match_start)
-    and records for match_result (match_id, player_id, result) for
-    each player.
+    Full set of records for a match consists of records for:
+        match (id, player_id_1, player_id_2, tournament_id, match_start) and
+        records for match_result (match_id, player_id, result)
+        for each player.
 
     Args:
       winner: the id number of the player who won
       loser: the id number of the player who lost
-
     """
 
     DB = connect()
     c = DB.cursor()
 
+    # before recording results, create record for the match
     c.execute("INSERT INTO match (player_id_1, player_id_2, tournament_id, "
         "match_start) VALUES (%s, %s, 1, %s)", (winner, loser, datetime.now(),))
 
@@ -143,23 +121,23 @@ def reportMatch(winner, loser):
     DB.commit()
     DB.close()
 
-# also need overloaded version that is passed tournament_id and can then create
-    # match records for each round and associate them with the tournament
+
 def swissPairings():
     """
-    Returns a list of pairs of players for the next round of a match.
+    Draws the current player standings (ranked by wins) from the database and
+    creates pairs players for the next round of the match.
   
-    Assuming that there are an even number of players registered, each player
-    appears exactly once in the pairings. Each player is paired with another
-    player with an equal or nearly-equal win record, that is, a player adjacent
-    to him or her in the standings.
+    Assuming an even number of registered players, each player is paired with
+    exactly one other player. Each player appears exactly once in the
+    pairings. Each player is paired with the player closest in the standings
+    (with an equal or nearly-equal win record).
   
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
-        id1: the first player's unique id
-        name1: the first player's name
-        id2: the second player's unique id
-        name2: the second player's name
+        p1_id: the first player's unique id
+        p1_name: the first player's name
+        p2_id: the second player's unique id
+        p2_name: the second player's name
     """
 
     DB = connect()
@@ -170,10 +148,6 @@ def swissPairings():
 
     DB.close()
 
-    # if len(standings)%2 == 1
-        # assign a bye to the player with the most wins who hasn't already 
-        # had a bye
-    # elif
     pairings = []
     for n in range(len(standings)/2):
         p1 = standings.pop(0)
